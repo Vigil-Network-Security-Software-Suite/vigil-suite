49e8d0687036bde51d29d1004feca3db0c2df754
raging-loon <cxmacolley@gmail.com>
2022-06-03 09:51:34 -0400

    update readme

---
+++ vlconsole/main.c
@@ -0,0 +1,30 @@
+  #include <stdlib.h>
+  #include <curses.h>
+
+  int main(){
+    int lines, cols;
+    long iter;
+    int index;
+    int y[3], x[3];
+    initscr(); /* Initialize screen, size, feat, etx */ 
+    cbreak(); 
+    noecho();
+
+    clear();
+    lines = LINES - 1;
+    cols = COLS - 1;
+
+    x[0] = 0;
+    y[0] = 0;
+    
+    y[1] = lines;
+    x[1] = cols / 2;
+
+    y[2] = 0;
+    x[2] = cols;
+    
+
+    refresh();
+    getch();
+    endwin();
+  }
