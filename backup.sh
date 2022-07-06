432cc8eccfd0732db6177b0d854fd5c1f41a045f
raging-loon <cxmacolley@gmail.com>
2022-07-06 16:52:36 -0400

    added

--- backup.sh
+++ backup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 filename=$(pwd | sed 's/\// /g' | awk '{print $NF}').$(date +%m.%d.%y).tar
-echo $filename
 tar --exclude-vcs -zvcf $filename ./
 mv $filename ~/vns3-backups
 cd ~/vns3-backups
